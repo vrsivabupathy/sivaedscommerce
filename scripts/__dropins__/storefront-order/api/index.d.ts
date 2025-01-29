9af73e59ab869e4baa4eaa148413cb85e973e52e
vrsivabupathy <141131728+vrsivabupathy@users.noreply.github.com>
2025-01-29 11:42:49 +0530

    Initialize test-pack

---
+++ scripts/__dropins__/storefront-order/api/index.d.ts
@@ -0,0 +1,9 @@
+export * from './initialize';
+export * from './fetch-graphql';
+export * from './getOrderDetailsById';
+export * from './getGuestOrder';
+export * from './guestOrderByToken';
+export * from './getAttributesForm';
+export * from './getCustomer';
+export * from './getStoreConfig';
+//# sourceMappingURL=index.d.ts.map
