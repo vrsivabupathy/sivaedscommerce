2e9928911422f1bc507cd425f570aec9ed8e28e1
vrsivabupathy <141131728+vrsivabupathy@users.noreply.github.com>
2025-01-29 11:42:33 +0530

    Initialize storage-errors

---
+++ scripts/configs.js
@@ -0,0 +1,83 @@
+const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
+
+/**
+ * This function calculates the environment in which the site is running based on the URL.
+ * It defaults to 'prod'. In non 'prod' environments, the value can be overwritten using
+ * the 'environment' key in sessionStorage.
+ *
+ * @returns {string} - environment identifier (dev, stage or prod'.
+ */
+export const calcEnvironment = () => {
+  const { href } = window.location;
+  let environment = 'prod';
+  if (href.includes('.aem.page')) {
+    environment = 'stage';
+  }
+  if (href.includes('localhost')) {
+    environment = 'dev';
+  }
+  try {
+    const environmentFromConfig = window.sessionStorage.getItem('environment');
+    if (environmentFromConfig && ALLOWED_CONFIGS.includes(environmentFromConfig) && environment !== 'prod') {
+      return environmentFromConfig;
+    }
+  } catch (e) {
+    // Do nothing
+  }
+
+  return environment;
+};
+
+function buildConfigURL(environment) {
+  const env = environment || calcEnvironment();
+  let fileName = 'configs.json?sheet=prod';
+  if (env !== 'prod') {
+    fileName = `configs-${env}.json`;
+  }
+  const configURL = new URL(`${window.location.origin}/${fileName}`);
+  return configURL;
+}
+
+const getConfigForEnvironment = async (environment) => {
+  const env = environment || calcEnvironment();
+  let configJSON;
+
+  try {
+    configJSON = window.sessionStorage.getItem(`config:${env}`);
+    if (!configJSON) {
+      throw new Error('No config in sessionStorage');
+    }
+  } catch (e) {
+    // Do nothing
+  }
+
+  if (!configJSON) {
+    configJSON = await fetch(buildConfigURL(env));
+    if (!configJSON.ok) {
+      throw new Error(`Failed to fetch config for ${env}`);
+    }
+    configJSON = await configJSON.text();
+
+    try {
+      window.sessionStorage.setItem(`config:${env}`, configJSON);
+    } catch (e) {
+      // Do nothing
+    }
+  }
+
+  return configJSON;
+};
+
+/**
+ * This function retrieves a configuration value for a given environment.
+ *
+ * @param {string} configParam - The configuration parameter to retrieve.
+ * @param {string} [environment] - Optional, overwrite the current environment.
+ * @returns {Promise<string|undefined>} - The value of the configuration parameter, or undefined.
+ */
+export const getConfigValue = async (configParam, environment) => {
+  const env = environment || calcEnvironment();
+  const configJSON = await getConfigForEnvironment(env);
+  const configElements = JSON.parse(configJSON).data;
+  return configElements.find((c) => c.key === configParam)?.value;
+};
