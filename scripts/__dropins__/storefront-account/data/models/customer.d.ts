dcb8520343c8c75728ddb98e18b06037b980c013
vrsivabupathy <141131728+vrsivabupathy@users.noreply.github.com>
2025-01-29 11:43:09 +0530

    Initialize revert-186-cart-attribute-slot

---
+++ scripts/__dropins__/storefront-account/data/models/customer.d.ts
@@ -0,0 +1,14 @@
+export interface CustomerDataModelShort {
+    firstName: string;
+    lastName: string;
+    middleName: string;
+    dateOfBirth: string;
+    dob: string;
+    prefix: string;
+    gender: 1 | 2 | string;
+    suffix: string;
+    email: string;
+    createdAt: string;
+    [key: string]: string | boolean | number;
+}
+//# sourceMappingURL=customer.d.ts.map
