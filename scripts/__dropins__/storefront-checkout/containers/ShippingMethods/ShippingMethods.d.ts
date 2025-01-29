071714623dc545df3338b278140cd6dee5840ffb
vrsivabupathy <141131728+vrsivabupathy@users.noreply.github.com>
2025-01-29 11:43:07 +0530

    Initialize GA-Demo-Develop

---
+++ scripts/__dropins__/storefront-checkout/containers/ShippingMethods/ShippingMethods.d.ts
@@ -0,0 +1,18 @@
+import { ShippingMethod } from '../../data/models';
+import { SlotProps } from '@dropins/tools/types/elsie/src/lib';
+import { HTMLAttributes } from 'preact/compat';
+
+export interface ShippingMethodsProps extends HTMLAttributes<HTMLDivElement> {
+    onCheckoutDataUpdate?: () => Promise<void>;
+    onShippingMethodSelect?: (method: ShippingMethod) => void;
+    preSelectedMethod?: {
+        carrierCode: string;
+        methodCode: string;
+    };
+    shippingMethodsSlot?: SlotProps;
+}
+export declare const ShippingMethods: {
+    ({ hideOnEmptyCart, hideOnVirtualCart, ...props }: import('../../hocs/withConditionalRendering').ConditionalProps & ShippingMethodsProps): import("preact/compat").JSX.Element;
+    displayName: string;
+};
+//# sourceMappingURL=ShippingMethods.d.ts.map
