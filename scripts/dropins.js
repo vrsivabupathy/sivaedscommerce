4a8c8b18c7beaced56cedddfeb99c192d365f98d
vrsivabupathy <141131728+vrsivabupathy@users.noreply.github.com>
2025-01-29 11:42:36 +0530

    Initialize USF-1429-Checkout-in-EDS-blocks

---
+++ scripts/dropins.js
@@ -0,0 +1,83 @@
+/* eslint-disable import/no-unresolved */
+
+// Drop-in Tools
+import { events } from '@dropins/tools/event-bus.js';
+import {
+  removeFetchGraphQlHeader,
+  setEndpoint,
+  setFetchGraphQlHeader,
+} from '@dropins/tools/fetch-graphql.js';
+import { initializers, Initializer } from '@dropins/tools/initializer.js';
+
+// Drop-ins
+import * as authApi from '@dropins/storefront-auth/api.js';
+import * as cartApi from '@dropins/storefront-cart/api.js';
+import * as checkoutApi from '@dropins/storefront-checkout/api.js';
+import * as orderConfirmationApi from '@dropins/storefront-order-confirmation/api.js';
+
+// Recaptcha
+import * as recaptcha from '@dropins/tools/recaptcha.js';
+
+// Libs
+import { getConfigValue, getCookie } from './configs.js';
+
+export const getUserTokenCookie = () => getCookie('auth_dropin_user_token');
+
+// Update auth headers
+const setAuthHeaders = (state) => {
+  if (state) {
+    const token = getUserTokenCookie();
+    setFetchGraphQlHeader('Authorization', `Bearer ${token}`);
+  } else {
+    removeFetchGraphQlHeader('Authorization');
+  }
+};
+
+const persistCartDataInSession = (data) => {
+  if (data?.id) {
+    sessionStorage.setItem('DROPINS_CART_ID', data.id);
+  } else {
+    sessionStorage.removeItem('DROPINS_CART_ID');
+  }
+};
+
+const initialize = new Initializer({
+  init: () => {
+    // on page load, check if user is authenticated
+    const token = getUserTokenCookie();
+    // set auth headers
+    setAuthHeaders(!!token);
+    // emit authenticated event if token has changed
+    events.emit('authenticated', !!token);
+  },
+  listeners: () => [
+    // Set auth headers on authenticated event
+    events.on('authenticated', setAuthHeaders),
+    // Cache cart data in session storage
+    events.on('cart/data', persistCartDataInSession, { eager: true }),
+  ],
+});
+
+export default async function initializeDropins() {
+  // Register Initializers (Global)
+  initializers.register(initialize, {});
+  initializers.register(authApi.initialize, {});
+  initializers.register(cartApi.initialize, {});
+  initializers.register(checkoutApi.initialize, {});
+  initializers.register(orderConfirmationApi.initialize, {});
+
+  const mount = async () => {
+    // Event Bus Logger
+    events.enableLogger(true);
+    // Set Fetch Endpoint (Global)
+    setEndpoint(await getConfigValue('commerce-core-endpoint'));
+    // Recaptcha
+    recaptcha.setConfig();
+    // Mount all registered drop-ins
+    initializers.mount();
+  };
+
+  // Mount Drop-ins
+  window.addEventListener('pageshow', mount);
+  document.addEventListener('prerenderingchange', mount);
+}
